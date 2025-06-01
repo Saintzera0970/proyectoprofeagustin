f772749052a99de0fe52a0e6eb4afe02ab5615a9
Facundo080808 <facundo_hub@hotmail.com>
2025-06-01 18:13:53 -0300

    servidor funcionando

--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,10 @@
 import { Router } from "express" ;
 import {productsRouter} from './modules/productos/router/product_router.js'
 import {VentasRouter} from './modules/ventas/router/router.js'
+import { ruta } from "./modules/clientes/router/indexRoutes.js";
+
 export const router = Router();
 
 router.use('/productos', productsRouter);
 router.use('/ventas', VentasRouter);
-
+router.use('/clientes',ruta)

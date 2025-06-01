f0180289e6133dc5c8ad5dbdeaa9a29c2551ee70
Facundo080808 <facundo_hub@hotmail.com>
2025-06-01 14:19:47 -0300

    Cambios en la estructura de productos

--- backend/src/productos/router/product_router.js
+++ backend/src/productos/router/product_router.js
@@ -1,22 +1,13 @@
 import express from 'express';
 import db from '../../conection.js';
-
-
-
-const {productos} = db;
+import { CreateProductHandler, DeleteProductHandler, GetAllProductHandler, GetByIdProductHandler, PutProductHandler } from '../handler/product_handler.js';
 
 export const productsHandler = express.Router();
 
-
 productsHandler.get('/',async(req,res)=>{
     try {
-        const response = await productos.findAll();
-        const ordered = response.sort((a,b)=>a.name.toLowerCase().localeCompare(b.name.toLowerCase()));
-        if (!response) {
-            res.status(400).send('error')
-        }
-        
-        return res.status(200).json(ordered);
+         const response = await GetAllProductHandler()
+         res.status(200).json(response)
     } catch (error) {
         return res.status(500).json({error:error.message});
     }
@@ -24,21 +15,17 @@ productsHandler.get('/',async(req,res)=>{
 productsHandler.get('/:id',async(req,res)=>{
     const {id} = req.params;
     try {
-        const response = await productos.findOne({where:{id}});
-        if (!response) {
-            return res.status(404).send('producto no encontrado')
-        }
-            return res.status(200).json(response);
+       const response = await GetByIdProductHandler(id)
+       res.status(200).json(response)
     } catch (error) {
         return res.status(500).json({error:error.message})
     }
 });
 
 productsHandler.post('/',async(req,res)=>{
-    const {brand,name,category,price,stock,wholPrice,codeBar} = req.body;
     try {
-        const response = await productos.create({brand,name,category,price,stock,wholPrice,codeBar});
-        return res.status(200).json(response);
+        const response = await CreateProductHandler(req.body)
+        res.status(201).json(response)
     } catch (error) {
         return res.status(500).json({error:error.message})
     }
@@ -46,17 +33,9 @@ productsHandler.post('/',async(req,res)=>{
 
 productsHandler.put('/:id',async (req,res)=>{
     const {id}= req.params;
-    const {brand,name,category,price,wholPrice,stock} =req.body;
     try {
-        const product = await productos.findOne({where:{id}})
-        if (!product) {
-            return res.status(404).send('producto no encontrado')
-        }
-        const response = await productos.update(
-            { brand, name, category, price, wholPrice, stock }, 
-            { where: { id } } 
-          );
-        return res.status(201).json(response);
+        const response =  await PutProductHandler(id,req.body)
+        res.status(201).json(response)
     } catch (error) {
         return res.status(500).json({error:error.message})
     }
@@ -65,12 +44,8 @@ productsHandler.put('/:id',async (req,res)=>{
 productsHandler.delete('/:id',async(req,res)=>{
     const {id} = req.params;
     try {
-        const producto = await productos.findOne({where:{id}});
-        if (!producto) {
-            return res.status(404).send('producto no encontrado')
-        }
-        const response = await productos.destroy({where:{id}});
-        return res.status(200).send('producto eliminado' + response)
+       const response = await DeleteProductHandler(id)
+       res.status(204).send(response)
     } catch (error) {
         return res.status(500).json({error:error.message});
     }
